646549d0853ab288a48af7d5965485c571236028
sanghwa1225.lee <sanghwa1225.lee@sk.com>
2016-12-21 12:01:06 +0900

    oauth temp for semina

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ var path = require('path');
 global.appRoot = path.resolve(__dirname);
 global.modulePath = global.appRoot + "/module"
 
-var express = require('express');
-var app = express();
-var streamHelper = new (require('./module/streamHelper.js')).streamHelper();
-var fileLogger = new (require('./module/fileLogger.js')).fileLogger();
+const express = require('express');
+const app = express();
+const streamHelper = new (require('./module/streamHelper.js')).streamHelper();
+const fileLogger = new (require('./module/fileLogger.js')).fileLogger();
+const bodyParser = require('body-parser');
 
 // initialize
 app.set('views', './views');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/resource/', express.static(__dirname + '/resource'));
 app.use('/js/', express.static(__dirname + '/js'));
 app.use('/css/', express.static(__dirname + '/css'));
 
-var server = app.listen(80, function() {
+var server = app.listen(8080, function() {
   var port = server.address().port;
   process.on('uncaughtException', function (err) {
     fileLogger.error('uncaughtException : ' + err);
@@ -43,4 +45,9 @@ app.get('/pseudo/:movie', (req, res) => {
 
 app.get('/webTorrentDemo', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/webTorrentDemo.html'));
+});
+
+// 임시
+app.get('/oauth', (req, res) => {
+  res.status(200).send(req.query);
 });
